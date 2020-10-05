302cfc5811482d8d54c0a70d718a9e5cef5be370
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-10-04 20:26:51 -0400

    Add files via upload

--- functions/qtRead03.js
+++ functions/qtRead03.js
@@ -1,7 +1,36 @@
+
+// read one question by myId from qtQuestions collection.
+let myFaunaCollection = 'qtQuestions'
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+
 exports.handler = (event, context) => {
-  console.log('hello from qtRead03.js')
-  return {
-    statusCode: 200,
-    body: 'wingo'
-  }
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  }) 
+  // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
+ 
+  // let myFaunaCollection = 'qtQuestions'
+  let myFaunaRecId =  '276380634185728512'
+  let myFaunaFetchRef = 'classes/' + myFaunaCollection + '/'+ myFaunaRecId 
+  console.log('Function read03 myFaunaFetchRef : ' + myFaunaFetchRef) 
+
+  
+  return client.query(q.Get(q.Ref(myFaunaFetchRef)))
+    .then((response) => {
+      console.log('success', response)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }
+    }).catch((error) => {
+      console.log('error', error)
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
 }
