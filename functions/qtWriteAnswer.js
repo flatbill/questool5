84554ed1d9eac343d32db88791669d666b283257
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-10-18 18:56:51 -0400

    Add files via upload

--- functions/qtWriteAnswer.js
+++ functions/qtWriteAnswer.js
@@ -22,6 +22,14 @@ exports.handler = async (event, context) => {
       /* Success! return the response with statusCode 200 */
       return {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*' 
+        //  'Access-Control-Allow-Origin': 'http://localhost:4200' 
+        // netlify allows only one allow-origin.  
+        // this wrecks the query string fetch.
+        // so, I set this to '*' which is hopefully temp,
+        // until I can figure it out.
+        },
         body: JSON.stringify(response)
       }
     }).catch((error) => {
