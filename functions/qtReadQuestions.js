03ac2a6f6e351fb430b5c9b231fcc1cd82cb7029
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-10-20 17:18:00 -0400

    Add files via upload

---
+++ functions/qtReadQuestions.js
@@ -0,0 +1,46 @@
+
+// read  questions by qid.
+let myFaunaCollection = 'qtQuestions'
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+exports.handler = (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  }) 
+  const id = getIdInline(event.path)
+
+  console.log('Netlify Function qtReadQuestions invoked.')
+  // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
+  
+  let myFaunaFetchQuests = 'classes/' + myFaunaCollection + '/'
+  return client.query(q.Paginate(q.Match(q.Ref('indexes/qxQuestionsX1'))))
+    .then((response) => {
+    const todoRefs = response.data
+    console.log('Todo refs', todoRefs)
+    console.log(`${todoRefs.length} todos found`)
+    const getAllTodoDataQuery = todoRefs.map((ref) => {
+      return q.Get(ref)
+    })
+    // then query the refs
+    return client.query(getAllTodoDataQuery).then((ret) => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(ret)
+      }
+    })
+  }).catch((error) => {
+    console.log('error', error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  })  
+}
+function getIdInline(urlPath) {
+  console.log(`Running getIdInline from Netlify Function 'qtRead03' . urlPath: ${urlPath}`)
+
+  return urlPath.match(/([^\/]*)\/*$/)[0]
+}
