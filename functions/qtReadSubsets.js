ef2219e634c4d64049da0f66760b8815c5dde9d7
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-10-25 18:43:29 -0400

    Add files via upload

--- functions/qtReadSubsets.js
+++ functions/qtReadSubsets.js
@@ -4,7 +4,7 @@ let myFaunaCollection = 'qtSubsets'
 const faunadb = require('faunadb')
 const q = faunadb.query
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
   /* configure faunaDB Client with our secret */
   const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET2
@@ -17,17 +17,17 @@ exports.handler = (event, context) => {
   // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
   
 /* parse the string body into a useable JS object */
-//const datafromEventBody = JSON.parse(event.body)
+const datafromEventBody = JSON.parse(event.body)
 //console.log('Function qtReadSubsets invoked. event body: ')
 //console.table(event.body) 
-// const todoItem = {
-//   datafromEventBody: data
-// }
+const todoItem = {
+  datafromEventBody: data
+}
 //return client.query(q.Create(q.Ref('classes/todos'), todoItem))
 
   let myFaunaFetchQuests = 'classes/' + myFaunaCollection + '/'
   // return client.query(q.Paginate(q.Match(q.Ref('indexes/qtSubsetsX1'))))
-  return client.query(q.Paginate(q.Match(q.Index('qtSubsetsX1'), '1' )))
+  return client.query(q.Paginate(q.Match(q.Index('qtSubsetsX1'), todoItem )))
   //q.Match(q.Index('spells_by_element'), 'fire')
     .then((response) => {
     const todoRefs = response.data
