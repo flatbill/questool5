2c0d70a84999996bca861a70b2c190beebffcbba
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2020-11-12 17:28:23 -0500

    Add files via upload

---
+++ functions/qtMassDeleteScores.js
@@ -0,0 +1,44 @@
+
+let myFaunaCollection = 'qtScores'
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+exports.handler = async (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  }) 
+  const myCust = event.queryStringParameters.cust 
+  const myQid = event.queryStringParameters.qid 
+  console.log('myCust:',myCust)
+  console.log('myQid:',myQid)
+  console.log('Netlify Function qtDeleteMassScores invoked.')
+  console.log('here is qtDeleteMassScores event:', event)
+  //
+// construct batch query from a list of Ids
+
+let refs = await client.query
+  (q.Paginate(q.Match(q.Index('qtScoresX1'),[myCust,myQid]))
+  )
+let listOfIds = refs.data.map((r) => r.id)
+ const deleteMassQuery = listOfIds.map((idd) => {
+   return q.Delete(q.Ref(`classes/qtScores/${idd}`))
+})
+  return client.query(deleteMassQuery)
+    .then((response) => {
+      console.log('running .then of qtDeleteMassScores')
+      return {
+        statusCode: 200,
+        headers: {'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(response)
+      }
+    })
+   .catch((error) => {
+    console.log('error running qtDeleteMassScores', error)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    }
+  })  
+}
