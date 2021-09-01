00f2d6bfce9c15e8312a2c5f88866a8f99f34959
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2021-09-01 16:38:12 -0400

    Add files via upload

---
+++ functions/qtDeleteScoreboard.js
@@ -0,0 +1,56 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+/* export our lambda function as named "handler" export */
+exports.handler = async (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  })  
+  let myCust = "1" //event.queryStringParameters.cust 
+  let myQid = "1" //event.queryStringParameters.qid 
+  let myscoreboardNbr = '1'   
+
+  /* parse the string body input into a useable JS object */
+  const dataIn = JSON.parse(event.body)
+  console.log('Function qtDeleteQuestion invoked. dataIn: ', dataIn)
+  myCust     = dataIn.cust
+  myQid      = dataIn.qid
+  myScoreboardNbr = dataIn.scoreboardNbr
+  let queryResult1 = await client.query
+  (q.Get(q.Match(q.Index('qtScoreboardsX2'),[myCust,myQid,myscoreboardNbr])))
+  //console.log('pgm change 2/2/2021 8:26')
+  console.log('queryResult1.ref: ')
+  console.log(queryResult1.ref)
+
+  // const scoreboardAdelic = {
+  //   data: data //not used for this function
+  // }
+  
+  /* construct the fauna query */
+  return client.query(q.Delete(q.Ref(queryResult1.ref)))
+    .then((response) => {
+      console.log('success', response)
+      /* Success! return the response with statusCode 200 */
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*' 
+        //  'Access-Control-Allow-Origin': 'http://localhost:4200' 
+        // netlify allows only one allow-origin.  
+        // this wrecks the query string fetch.
+        // so, I set this to '*' which is hopefully temp,
+        // until I can figure it out.
+        },
+        body: JSON.stringify(response)
+      }
+    }).catch((error) => {
+      console.log('error', error)
+      /* Error! return the error with statusCode 400 */
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
---
+++ functions/qtUpdateScoreboard.js
@@ -0,0 +1,56 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const q = faunadb.query
+
+/* export our lambda function as named "handler" export */
+exports.handler = async (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET2
+  })  
+  let myCust = "1" //event.queryStringParameters.cust 
+  let myQid = "1" //event.queryStringParameters.qid 
+  let myScoreboardNbr = '1'   
+
+  /* parse the string body input into a useable JS object */
+  const data = JSON.parse(event.body)
+  console.log('Function qtUpdateScoreboard invoked. data: ', data)
+  myCust     = data.cust
+  myQid      = data.qid
+  myScoreboardNbr = data.scoreboardNbr
+  let queryResult1 = await client.query
+  (q.Get(q.Match(q.Index('qtScoreboardsX2'),[myCust,myQid,myScoreboardNbr])))
+  //console.log('pgm change 2/2/2021 8:26')
+  console.log('queryResult1.ref: ')
+  console.log(queryResult1.ref)
+
+   const scoreboardAdelic = {
+     data: data 
+   }
+  
+  /* construct the fauna query */
+  return client.query(q.Update(q.Ref(queryResult1.ref),scoreboardAdelic))
+    .then((response) => {
+      console.log('success', response)
+      /* Success! return the response with statusCode 200 */
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*' 
+        //  'Access-Control-Allow-Origin': 'http://localhost:4200' 
+        // netlify allows only one allow-origin.  
+        // this wrecks the query string fetch.
+        // so, I set this to '*' which is hopefully temp,
+        // until I can figure it out.
+        },
+        body: JSON.stringify(response)
+      }
+    }).catch((error) => {
+      console.log('error', error)
+      /* Error! return the error with statusCode 400 */
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
