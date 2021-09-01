804ee6aaa454c5dd33b6e264a8d2978f9d0dad7b
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2021-09-01 17:28:49 -0400

    Add files via upload

--- functions/qtAddScoreboard.js
+++ functions/qtAddScoreboard.js
@@ -16,7 +16,7 @@ exports.handler = async (event, context) => {
     data: data
   }
   /* construct the fauna query */
-  return client.query(q.Create(q.Ref('classes/qtScoreboard'), scoreboardAdelic))
+  return client.query(q.Create(q.Ref('classes/qtScoreboards'), scoreboardAdelic))
     .then((response) => {
       console.log('success', response)
       /* Success! return the response with statusCode 200 */
