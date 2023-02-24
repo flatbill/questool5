dee6f2359349704bf93b63bbfdc8f8c51aa16726
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-02-24 07:06:54 -0500

    Add files via upload

--- functions/qtReadUsers.js
+++ functions/qtReadUsers.js
@@ -1,8 +1,8 @@
 
 let myFaunaCollection = 'qtUsers'
-/* Import faunaDB sdk */
+/* Import faunaDB sdk */ 
 const faunadb = require('faunadb')
-const supabaser = require('supabase')
+// const supabaser = require('supabase')
 const q = faunadb.query
 
 exports.handler = (event, context) => {
