9cb37f5a87bf7ba23c588688b19c1e8693ce4769
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-03-30 18:45:36 -0400

    Add files via upload

--- functions/qtReadMultSubscribers.js
+++ functions/qtReadMultSubscribers.js
@@ -11,16 +11,16 @@ exports.handler = (event, context) => {
   }) 
 //   const mySubscriber = event.queryStringParameters.subscriber
 
-  console.log('Netlify Function qtReadSubscribers invoked.')
+  console.log('Netlify Function qtReadMultSubscribers invoked.')
 //   console.log(mySubscriber)
   // let myFaunaDbName = 'alaska6'  // not needed cuz FAUNADB_SERVER_SECRET2 is tied to database alaska6.
   
-  let myFaunaFetchSubscribers = 'classes/' + myFaunaCollection + '/'
+  // let myFaunaFetchSubscribers = 'classes/' + myFaunaCollection + '/'
 //   return client.query(q.Paginate(q.Match(q.Index('qtSubscribersX1'),[mySubscriber]),{ size: 1 }))
-return client.query(q.Paginate(q.Documents(q.Collection('qtSubscribers')),{ size: 1 }))
+return client.query(q.Paginate(q.Documents(q.Collection('qtSubscribers')),{ size: 55 }))
     .then((response) => {
     const qtSubscribersStuff = response.data
-    console.log('qtSubscribers stuff:', qtSubscribersStuff)
+    console.log('23 qtSubscribers stuff:', qtSubscribersStuff)
     console.log(`${qtSubscribersStuff.length} qtSubscribers found`)
     return {
         statusCode: 200,
