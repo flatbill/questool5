f2ba0929dde60cccb383ab8a6d2e4fb390c21d02
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2023-05-22 13:29:36 -0400

    Add files via upload

--- functions/getFaunaOrSupabase.js
+++ functions/getFaunaOrSupabase.js
@@ -1,5 +1,8 @@
 // const faunaOrSupabase = process.env.faunaOrSupabase
 const faunaOrSupabase = 'fauna'
+let responseBody = {
+  message: 'hi from message'
+};
 
 /* export our lambda function as named "handler" export */
 exports.handler = async (event, context) => {
@@ -8,6 +11,6 @@ exports.handler = async (event, context) => {
       return {
         statusCode: 200,
         headers: {  'Access-Control-Allow-Origin': '*'  },
-        body: faunaOrSupabase
+        body:    JSON.stringify(responseBody)
       }
 }
